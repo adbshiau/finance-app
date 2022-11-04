bd7307b47f28be5bf329b636d49af736600aa26f
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2022-11-04 13:31:38 -0700

    Define scripts in package.json

--- server/server.js
+++ server/server.js
@@ -7,10 +7,14 @@ const app = express();
 // store port as variable
 const port = process.env.PORT || 3001;
 
+// routes
 app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
+  res.status(404).json({
+    users: ["userOne", "userTwo", "userThree"],
+  });
 });
 
+// tells express to listen on a specific port
 app.listen(port, () => {
   // call back function
   console.log(`Server listening on ${port}!`);
